82d4e445500819d64417f2f2bb9ced0ec108672f
whimax07 <59751270+whimax07@users.noreply.github.com>
2022-01-04 21:06:54 +0000

    Fixed a bug that two groups where equal if they had the same contents.

--- src/utils/Group.java
+++ src/utils/Group.java
@@ -1,6 +1,10 @@
 package utils;
 
-import utils.E_CharClass;
-
 public record Group(String chars, boolean isRepeatable, E_CharClass charClass) {
+
+    @Override
+    public boolean equals(Object obj) {
+        return this == obj;
+    }
+
 }
